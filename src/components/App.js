1e54d6411a9e1218d2af887c855434ce369dfa17
Rashmit Pankhania <rashmitpankhania@gmail.com>
2020-05-24 00:47:02 +0530

    added Appbar Header.js

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './ui/Header';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Header/>
+            hello!!
+        </div>
+    );
+};
+
+export default App;
