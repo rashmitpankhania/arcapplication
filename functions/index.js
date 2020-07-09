13afde04a88382434c31a025a3d9fca004764362
Rashmit Pankhania <rashmitpankhania@gmail.com>
2020-07-09 12:36:44 +0530

    working email for the dummy email

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
-const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const functions  = require('firebase-functions');
+
+const config = functions.config();
+admin.initializeApp();
+const transporter = nodemailer.createTransport({service: 'Gmail', auth: {user: config.user.email, pass: config.user.password }});
+
+const mailToOptions = {from: 'Arc Development', to: 'sirruggedballs@outlook.com', subject: 'testing', text: 'testing'};
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.sendMail = functions.https.onRequest((request, response) => {
+    transporter.sendMail(mailToOptions, error => {
+      if(error)
+        response.send(error);
+      else
+        response.send("Message sent Successfully!!");
+    })
+});
