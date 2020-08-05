d7cf5dd20a6f08a457427acab67d369447bb4365
Rashmit Pankhania <rashmitpankhania@gmail.com>
2020-08-05 12:58:27 +0530

    server.js- port updation

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(9000);
+app.listen(port);
